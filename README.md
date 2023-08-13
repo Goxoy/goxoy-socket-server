115ddf03259466a5e6bd5628fcdc68aa5da6ca25
Omer GOKSOY <goksoy.omer@gmail.com>
2023-08-13 23:15:59 +0300

    çalışır hale getirildi

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Goxoy Socket Server
 
-Sunucu Soket kitaplığı. rust fonksiyonları kullanılarak geliştirilmiştir.
+Sunucu Soket kitaplığı. Rust fonksiyonları kullanılarak geliştirilmiştir.
 
 ## Kullanım / Örnekler
 
@@ -13,6 +13,33 @@ Sunucu Soket kitaplığı. rust fonksiyonları kullanılarak geliştirilmiştir.
         IPAddressVersion::IpV4,
     );
 
+    //mesaj geldiğinde devreye girecek fonksiyon
+    server_obj.on_receive(|sender_addr,income_data| {
+        let vec_to_string = String::from_utf8(income_data.clone()).unwrap();
+        println!("income callback => {} [ {} ]: {}", sender_addr, income_data.len(), vec_to_string);
+    });
+
+    // yeni kullanıcı bağlandığında devreye girecek fonksiyon
+    server_obj.on_new_client(move |on_new_client| {
+        println!("new client connected : {}", on_new_client);
+    });
+
+    // kullanıcının bağlantısı koptuğunda devreye girecek fonksiyon
+    server_obj.on_client_disconnect(move |on_new_client| {
+        println!("client disconnected : {}", on_new_client);
+    });
+
+    // işlem hatası olduğunda devreye girecek fonksiyon
+    server_obj.on_error(|data| {
+        println!("on error : {:?}", data);
+    });
+
+    //sunucuya başlatan komut
+    server_obj.start();
+    
+    // istenen istemciye mesaj gönderecek fonksiyon
+    server_obj.send(String::from("127.0.0.1:1234"), "welcome".as_bytes().to_vec());
+    
 ```
 
   
--- src/socket_server.rs
+++ src/socket_server.rs
@@ -1,11 +1,11 @@
-#![allow(warnings, unused)]
+//#![allow(warnings, unused)]
 use std::{
     io::{ErrorKind, Read, Write},
     net::TcpListener,
-    sync::mpsc,
+    sync::mpsc::{self},
     thread,
 };
-use std::sync::mpsc::{Sender, Receiver};
+use std::sync::mpsc::Sender;
 
 use goxoy_address_parser::address_parser::{AddressParser, IPAddressVersion, ProtocolType};
 
@@ -22,20 +22,25 @@ pub enum SocketServerStatus {
     Disconnected,
 }
 
+/*
 #[derive(Clone, Debug)]
 pub struct SocketServerMessageList {
     peer_addr:String,
     data:Vec<u8>
 }
+*/
+
 #[derive( Debug)]
 pub struct SocketServer {
     url: String,
+    local_addr_obj:AddressParser,
     started: bool,
     defined: bool,
     tx:Option<Sender<Vec<u8>>>,
     pub local_addr: String,
-    fn_receive_data: Option<fn(Vec<u8>)>,
+    fn_receive_data: Option<fn(String,Vec<u8>)>,
     fn_new_client: Option<fn(String)>,
+    fn_disconnet_client: Option<fn(String)>,
     fn_error: Option<fn(SocketServerErrorType)>,
     buffer_size: usize,
 }
@@ -45,11 +50,13 @@ impl SocketServer {
         SocketServer {
             url: String::new(),
             local_addr: String::new(),
+            local_addr_obj:AddressParser { ip_address: String::new(), port_no: 0, protocol_type: ProtocolType::TCP, ip_version: IPAddressVersion::IpV4 },
             tx:None,
             started: false,
             defined: false,
             fn_receive_data: None,
             fn_new_client: None,
+            fn_disconnet_client: None,
             fn_error: None,
             buffer_size: 1024,
         }
@@ -60,25 +67,28 @@ impl SocketServer {
         port_no: usize,
         ip_version: IPAddressVersion,
     ) -> Self {
-        let local_addr = AddressParser::object_to_string(AddressParser {
-            ip_address: ip_address,
-            port_no: port_no,
-            protocol_type: protocol_type,
-            ip_version: ip_version,
-        });
+        let addr_obj=AddressParser {
+            ip_address,
+            port_no,
+            protocol_type,
+            ip_version,
+        };
         SocketServer {
             url: String::new(),
-            local_addr: local_addr,
+            local_addr: AddressParser::object_to_string(addr_obj.clone()),
+            local_addr_obj: addr_obj,
             tx:None,
             defined: true,
             started: false,
             fn_receive_data: None,
             fn_new_client: None,
+            fn_disconnet_client: None,
             fn_error: None,
             buffer_size: 1024,
         }
     }
     pub fn set_config(&mut self, config: AddressParser) {
+        self.local_addr_obj = config.clone();
         let local_addr = AddressParser::object_to_string(config);
         self.local_addr = local_addr;
         self.defined = true;
@@ -86,11 +96,14 @@ impl SocketServer {
     pub fn set_buffer_size(&mut self, buffer_size: usize) {
         self.buffer_size = buffer_size;
     }
-    pub fn on_receive(&mut self, on_receive_data: fn(Vec<u8>)) {
+    pub fn on_receive(&mut self, on_receive_data: fn(String,Vec<u8>)) {
         self.fn_receive_data = Some(on_receive_data);
     }
     pub fn on_new_client(&mut self, on_new_client: fn(String)) {
         self.fn_new_client = Some(on_new_client);
+    }
+    pub fn on_client_disconnect(&mut self, on_disconnet_client: fn(String)) {
+        self.fn_disconnet_client = Some(on_disconnet_client);
     }
     pub fn on_error(&mut self, on_error: fn(SocketServerErrorType)) {
         self.fn_error = Some(on_error);
@@ -119,37 +132,61 @@ impl SocketServer {
         if server.set_nonblocking(true).is_err(){
             return false;
         }
-        //let server=server.unwrap();
-
-        //println!("buraya geldi");
-        //std::process::exit(0);
-    
     
         let mut clients = vec![];
         let buffer_size=self.buffer_size;
         
         let (tx, rx) = mpsc::channel::<Vec<u8>>();
         self.tx=Some(tx.clone());
+
+        let on_disconnect_client_fn=self.fn_disconnet_client;
+        let on_receive_data=self.fn_receive_data;
+        let on_new_client_fn=self.fn_new_client;
+        let local_addr_obj_1=self.local_addr_obj.clone();
+        let local_addr_obj_2=self.local_addr_obj.clone();
+
         loop {
             if let Ok((mut socket, addr)) = server.accept() {
-                println!("Client {} connected", addr);
-    
+                if on_new_client_fn.is_some() {
+                    on_new_client_fn.unwrap()(
+                        AddressParser::binding_addr_to_string(
+                            addr.to_string(), 
+                            local_addr_obj_1.protocol_type, 
+                            local_addr_obj_1.ip_version
+                        )
+                    );
+                }
+                
                 let tx = tx.clone();
                 clients.push(socket.try_clone().expect("failed to clone client"));
                 thread::spawn(move || loop {
                     let mut buff = vec![0; buffer_size];
-    
-                    match socket.read_exact(&mut buff) {
+                    match socket.read(&mut buff) {
                         Ok(_) => {
                             let msg = buff.into_iter().take_while(|&x| x != 0).collect::<Vec<_>>();
-                            let msg = String::from_utf8(msg).expect("invalid utf8 message");
-    
-                            println!("{} {:?}", addr, msg);
+                            if on_receive_data.is_some(){
+                                on_receive_data.unwrap()(
+                                    AddressParser::binding_addr_to_string(
+                                        addr.to_string(), 
+                                        local_addr_obj_2.protocol_type, 
+                                        local_addr_obj_2.ip_version
+                                    ),
+                                    msg.clone()
+                                );
+                            }
                             tx.send(msg).expect("failed to send message to rx");
                         }
                         Err(ref err) if err.kind() == ErrorKind::WouldBlock => (),
                         Err(_) => {
-                            println!("Closing connection with: {}", addr);
+                            if on_disconnect_client_fn.is_some(){
+                                on_disconnect_client_fn.unwrap()(
+                                    AddressParser::binding_addr_to_string(
+                                        addr.to_string(), 
+                                        local_addr_obj_2.protocol_type, 
+                                        local_addr_obj_2.ip_version
+                                    )
+                                );
+                            }
                             break;
                         }
                     }
@@ -161,10 +198,8 @@ impl SocketServer {
                 clients = clients
                     .into_iter()
                     .filter_map(|mut client| {
-                        let mut buff = msg.clone().into_bytes();
-                        buff.resize(buffer_size, 0);
-    
-                        client.write_all(&buff).map(|_| client).ok()
+                        //println!("sent data");
+                        client.write_all(&msg).map(|_| client).ok()
                     })
                     .collect::<Vec<_>>();
             }
@@ -179,10 +214,7 @@ impl SocketServer {
         self.started = true;
         let addr_obj = AddressParser::string_to_object(self.local_addr.clone());
         if addr_obj.protocol_type == ProtocolType::TCP {
-            let mut bind_str = addr_obj.ip_address;
-            bind_str.push_str(":");
-            bind_str.push_str(&addr_obj.port_no.to_string());
-            self.url = bind_str.clone();
+            self.url = AddressParser::local_addr_for_binding(addr_obj);
             if self.fn_receive_data.is_none() {
                 println!("callback did not define");
             } else {
@@ -209,18 +241,22 @@ fn full_test() {
         IPAddressVersion::IpV4,
     );
     println!("server_obj.local_addr: {}", server_obj.local_addr);
-    server_obj.on_receive(|data| {
-        let vec_to_string = String::from_utf8(data.clone()).unwrap(); // Converting to string
-        println!("income callback [ {} ]: {}", data.len(), vec_to_string); // Output: Hello World
+    server_obj.on_receive(|sender,income_data| {
+        let vec_to_string = String::from_utf8(income_data.clone()).unwrap();
+        println!("income callback => {} [ {} ]: {}", sender, income_data.len(), vec_to_string);
     });
 
     server_obj.on_new_client(move |on_new_client| {
         println!("new client connected : {}", on_new_client);
+    });
+    server_obj.on_client_disconnect(move |on_new_client| {
+        println!("client disconnected : {}", on_new_client);
     });
     server_obj.on_error(|data| {
         println!("on error : {:?}", data);
     });
     server_obj.start();
     server_obj.send(String::from("127.0.0.1:1234"), "welcome".as_bytes().to_vec());
+    
     assert!(true)
 }
